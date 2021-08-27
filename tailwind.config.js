4efafa13d6d130442c7f3fd5e18a51883fdb3c88
Warchair <chairulsp2@gmail.com>
2021-08-28 00:00:36 +0700

    make a repository and adding file html tailwind css and image for website

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter':'Inter',
+        'open-sans':'Open Sans',
+        'poppins':'Poppins'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
